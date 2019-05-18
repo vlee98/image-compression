f10b9532d3ca99b49e72c926719b5518a866d18d
Vincent Lee <vincentlee1776@gmail.com>
2019-05-17 21:01:28 -0400

    finished compression

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
-# Image Compression (JPEG Variant)
+# Image Compression
 
 ## How It Works
     This uses fourier transforms. More specifically fast fourier transforms (FFT).
-    JPEG typically uses Discrete Cosine Transform, a variant of FFT. 
     FFT will bring the image to frequency space. To compress remove low frequencies and save the image. 
     To open/ show the filtered image, do the inverse FFT to bring the image back to the spatial domain.
+    JPEG compression is more involved than this but as one of it's steps it uses a variant of FFT called
+    Discrete Cosine Transform (DCT).
 
 ## Fast Fourier Transform
+    This is a N Log N solution to converting a signal into its frequencies. Regular Fourier Transform takes N^2 time. 
+    We rely on numpy fft library to acomplish this.
 
 ## Sources 
     Phenomenal Demo: http://madebyevan.com/dft/
--- frequency.py
+++ frequency.py
@@ -3,19 +3,48 @@ from skimage import io, img_as_ubyte, img_as_float32, color
 import matplotlib.pyplot as plt
 import sys
 
-def frequency(image):
+def frequency(image, percentage):
+    # convert to grayscale
     grayscale = color.rgb2gray(image)
-    ret = np.fft.fft2(grayscale)
-    ret = abs(np.log(np.fft.fftshift(ret)))
-    return ret
+    # frequencies of the image
+    f_transform = np.fft.fft2(grayscale)
+    
+    # this is just to display magnitude
+    f_shift = np.fft.fftshift(f_transform) 
+    magnitude = np.log(np.abs(f_shift))
+
+    max_element = max(map(max,abs(f_transform)))
+    # threshold
+    mask = abs(f_transform) <= max_element * percentage
+    
+    # Stats, counting removals is slo
+    """ 
+    removals = sum(map(sum,mask))
+    x, y = f_transform.shape 
+    count = x * y
+    print ("Shape :" , f_transform.shape)
+    print ("Removed :", removals)
+    print ("Percent Left : ", (count - removals) / count)
+"""
+    # "Compress" as in remove the pixels below frequency
+    f_transform[mask] = 0
+    ret = abs(np.fft.ifft2(f_transform))
+    return ret, grayscale, magnitude
 
 if __name__ == "__main__": 
+    if (len(sys.argv) !=3):
+        print ("Use Statement: file path and threshold percentage \nExample: python frequency.py data/rose.jpg .0001")
+        sys.exit(1)
     image_path = None
     try:
         image_path = sys.argv[1]
+        float(sys.argv[2])
     except:
-        print ("Error Image Path Invalid")
+        print ("Error Use Statement: file path and threshold percentage \nExample: python frequency.py data/rose.jpg .0001")
+        sys.exit(1)
     image = io.imread(image_path)
-    frequency_image = frequency(image)
-    plt.imshow(frequency_image, cmap = plt.get_cmap(name='gray'))
-    plt.show()
+    figure = plt.figure()
+    frequency_image, grayscale, magnitude = frequency(image, float(sys.argv[2]))
+    io.imsave('results/filtered.jpg', frequency_image)
+    io.imsave('results/grayscale.jpg', grayscale)
+    io.imsave('results/magnitude.jpg', magnitude)
